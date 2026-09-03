89ea6321d062d4c80aeb0757390056f3945a7b28
Bohdan Denysiuk <denisukbogdan413@gmail.com>
2026-09-03 19:41:56 +0200

    Add Task Manager application entry point

---
+++ src/main/java/com/bohdandenysiuk/taskmanager/TaskManagerApp.java
@@ -0,0 +1,8 @@
+package com.bohdandenysiuk.taskmanager;
+
+public class TaskManagerApp {
+
+	 public static void main(String[] args) {
+	        System.out.println("Task Manager started");
+	    }
+}
